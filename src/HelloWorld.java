5802658da4815f8838d3449f32a6c482d01d0a3b
FODIL Hocine <12208164@crit-xubuntu>
2023-03-24 11:19:29 +0100

    com

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,7 @@
 public class HelloWorld {
  
-	public static void main(String[] args) {
-		System.out.println("Ciao mondo !");
-		System.out.println("Olá mundo !");
+	public static void main(String[] args)
+		System.out.println("Hello World !");
 		System.out.println("Hola Mundo !");
 	}
  
