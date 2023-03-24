a02823e8fc4200ff0a5cf0ae86f12272ff997cf2
FODIL Hocine <12208164@crit-xubuntu>
2023-03-24 11:42:32 +0100

    merge

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,7 @@
 public class HelloWorld {
  
-<<<<<<< HEAD
-	public static void main(String[] args)
-=======
 	public static void main(String[] args) {
 		System.out.println("Ciao mondo !");
->>>>>>> 5ba02799c678f08c614f09e44a52ac6404675082
 		System.out.println("Hello World !");
 		System.out.println("Hola Mundo !");
 	}
